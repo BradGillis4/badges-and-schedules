44c18b9b2dae1bcbd5ca8eddaa3b096a3b169088
BradGillis4 <bradgillis4@gmail.com>
2020-12-09 17:37:28 -0500

    commit tree c5941f6a8027f55edf768a5e93bb18bad07a7534

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(guests)
+    arr = []
+
+    guests.each {|guest| arr.push(badge_maker(guest))}
+    return arr
+end
+
+def assign_rooms(guests)
+    arr1=[]
+
+    guests.each_with_index{|guest , index|
+     arr1.push("Hello, #{guest}! You'll be assigned to room #{index + 1}!")
+
+}
+return arr1
+end
+
+def printer(guests)
+    batch_badge_creator(guests).each {|guest| puts guest}
+    assign_rooms(guests).each {|guest| puts guest}
+end
